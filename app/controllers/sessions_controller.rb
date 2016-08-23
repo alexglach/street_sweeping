c2e58019688a9d7d76f16e60cd31902d0139876d
Alex Lach <alexglach@gmail.com>
2016-08-23 18:18:31 -0400

    log in page set up

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,20 @@
 class SessionsController < ApplicationController
 
+  def new
+    if signed_in_user?
+      redirect_to root_path
+    end
+  end
+
   def create
     @user = User.find_by_email(params[:email])
     if @user && @user.authenticate(params[:password])
       sign_in(@user)
       flash[:success] = "You have successfully signed in"
-      redirect_to user_path(current_user)
+      redirect_to request.referrer
     else
       flash[:danger] = "Incorrect log-in information. Please try again"
-      redirect_to root_path
+      render :new
     end
   end
 
